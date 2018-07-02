711004d71b34070a0ea40682a5d1ac256f0c83c2
Long Pun <long.pun@sands.com.mo>
2018-07-02 12:26:59 +0800

    Add schema and DataConcat

---
+++ schema.py
@@ -0,0 +1,12 @@
+schema = {
+    'iccid': str,
+    'used': float,
+    'plan_c': float,
+    'left': float,
+    'ac_time': str,
+    'state': str,
+    'product': str,
+    'company': str,
+    'carrier': str,
+    'plan_s': float
+}
